92abd41f94f1669b91a2f87eb2a8fa92976ee175
unknown <oklein01@gmail.com>
2019-08-28 21:34:25 +0300

    make it better

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(express.static('/views'))
+app.use(express.static('views'))
 
 router(app)
 
